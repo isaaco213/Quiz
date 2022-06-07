36fc1ca20b95321fecf8d3714049eec7df2ae687
Isaac Opina <1313685@edu.burnabyschools.ca>
2022-06-07 10:16:40 -0700

    best questions

--- Quiz/Quiz.cpp
+++ Quiz/Quiz.cpp
@@ -89,9 +89,11 @@ bool ask_question(string message, string answerOne, string answerTwo, int correc
 }
 
 int main()
-{
+{   
+    int score = 0;
     std::cout << "Welcome to the quiz, try to make it to the end or else.\n" << "\n";
-    ask_question("Is Morbius the best movie in existance?", "yes", "no", 1);
+    ask_question("Is Morbius the best movie in existance?", "no", "yes", 2);
+    ask_question("Is taxi 1998 the best movie?", "yes", "no", 1);
     ask_question("Is this a virus", "Yes", "No", 1); // message, answer one, answer two, correct answer (1..2)
     ask_question("Do you know how you contracted this virus?", "Yes", "No", 1); //message, answer one, answer two, correct answer (1..2)
     ask_question("Do you know when this virus was contracted?", "Yes", "No", 1); //message, answer one, answer two, correct answer (1..2)
