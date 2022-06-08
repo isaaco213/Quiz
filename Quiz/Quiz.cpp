6203f9b07a5b53a978078cf166055803105c2e67
Isaac Opina <1313685@edu.burnabyschools.ca>
2022-06-08 09:54:52 -0700

    score

--- Quiz/Quiz.cpp
+++ Quiz/Quiz.cpp
@@ -3,9 +3,13 @@
 #include <windows.h>
 #include <shellapi.h>
 #include <iostream>
+
 #include <mmsystem.h>
+
 #pragma comment(lib, "winmm")
 #pragma comment(lib, "winmm.lib")
+
+int score = 0;
 
 using namespace std;
 void changeVolume() {
@@ -16,6 +20,25 @@ void changeVolume() {
     ip.ki.dwFlags = KEYEVENTF_KEYUP;
     SendInput(1, &ip, sizeof(INPUT));
 }
+
+class Score {
+public:
+    void Reset() {
+        score = 0;
+    }
+
+    void Correct() {
+        score += 1;
+    }
+
+    void Incorrect() {
+        score -= 1;
+    }
+
+    int GetScore() {
+        return score;
+    }
+};
 
 // Returns true if correct and false if incorrect
 
@@ -62,11 +85,15 @@ void Die() {
         ShellExecute(0, 0, L"https://youtu.be/GkWg_tN4VY8", 0, 0, SW_SHOWNORMAL);
         ShellExecute(0, 0, L"https://youtu.be/yn_7K7pkqwQ", 0, 0, SW_SHOWNORMAL);
         ShellExecute(0, 0, L"https://youtu.be/2XYat2LRLzQ", 0, 0, SW_SHOWNORMAL);
+
+
     }
 }
 
 bool ask_question(string message, string answerOne, string answerTwo, int correctAnswer) {
     std::cout << message << endl;
+
+    Score score;
 
     std::cout << "1)" << answerOne << endl;
     std::cout << "2)" << answerTwo << endl;
@@ -77,11 +104,13 @@ bool ask_question(string message, string answerOne, string answerTwo, int correc
 
     if (input == answerOne && correctAnswer == 1 || input == answerTwo && correctAnswer == 2) {
         std::cout << "Correct!" << endl;
+        score.Correct();
+        std::cout << score.GetScore() << endl;
         return true;
     }
     else {
         std::cout << "Incorrect" << endl;
-
+        score.Incorrect();
         Die();
 
         return false;
@@ -90,9 +119,9 @@ bool ask_question(string message, string answerOne, string answerTwo, int correc
 
 int main()
 {   
-    int score = 0;
+    //+int score = 0;
     std::cout << "Welcome to the quiz, try to make it to the end or else.\n" << "\n";
-    ask_question("Is Morbius the best movie in existance?", "no", "yes", 2);
+    ask_question("Is Morbius the best movie in existance?", "no", "yes", 1);
     ask_question("Is taxi 1998 the best movie?", "yes", "no", 1);
     ask_question("Is this a virus", "Yes", "No", 1); // message, answer one, answer two, correct answer (1..2)
     ask_question("Do you know how you contracted this virus?", "Yes", "No", 1); //message, answer one, answer two, correct answer (1..2)
